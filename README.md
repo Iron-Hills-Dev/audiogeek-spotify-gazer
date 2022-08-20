8e7abfb1832127df22b0359eeae546b70c6d9e51
GALJO <jasiek.ole@gmail.com>
2022-08-20 20:53:43 +0200

    initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+# AudioGeek Spotify Gazer
---
+++ src/main/java/org/codebusters/audiogeek/spotifygazer/AudiogeekSpotifyGazerApplication.java
@@ -0,0 +1,13 @@
+package org.codebusters.audiogeek.spotifygazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiogeekSpotifyGazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiogeekSpotifyGazerApplication.class, args);
+	}
+
+}
